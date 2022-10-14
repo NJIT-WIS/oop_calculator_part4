244bdcaccadbc91a00aaeacec91a5245042f0200
kaw393939 <keith@webizly.com>
2022-10-14 11:49:06 -0400

    remove code

--- app/conversions/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This class converts data"""
-
-
-class Convert:
-    @staticmethod
-    def args_to_tuple(*args) -> tuple:
-        return args
--- app/exceptions/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Custom Exception Classes"""
-
-
-class OnlyOneValue(Exception):
-    def __init__(self, message="Cannot Perform Operation On Only One Value"):
-        self.message = message
-        super().__init__(self.message)
