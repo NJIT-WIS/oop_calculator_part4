ccc71a0ff21da58e6c504f738fab0c782b853073
kaw393939 <keith@webizly.com>
2022-10-18 11:52:48 -0400

    fix convert pylint

--- tests/test_convert_teacher.py
+++ tests/test_convert_teacher.py
@@ -4,7 +4,4 @@ from app.conversions import Convert
 
 def test_calculator_operations():
     """checks that convert args to tuple works"""
-    assert type(Convert.args_to_tuple(2, 2, 2)) == tuple
-
-
-
+    assert isinstance(Convert.args_to_tuple(2, 2, 2), tuple)
